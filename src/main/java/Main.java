0d580f0f3ae35195a9fa464e5f3867c68c48efe0
ETSphere <Elem174@proton.me>
2024-03-14 23:10:25 +0500

    Хоспаде как же это все коряво... нужно добавить проверку на 0 при получении делителя. забадяжить тесты. оптимизировать убогий код

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,8 @@
 import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.print("Введите делимое: ");
         int dividend = scanner.nextInt();
         System.out.print("Введите делитель: ");
@@ -25,13 +23,11 @@ class DivisionCalculator {
         int quotient = dividend / divisor;
         int remainder = dividend % divisor;
 
-
         int remaindTransfer = 0;
         StringBuilder steps = new StringBuilder();
         int dividendWidth = String.valueOf(dividend).length();
         int quotientWidth = String.valueOf(dividend).length();
         String line = widthLine(quotientWidth);
-
 
         for (int i = 0; i < dividendWidth; i++) {
             int nextDigit = Character.getNumericValue(String.valueOf(dividend).charAt(i)) + remaindTransfer;
@@ -40,51 +36,42 @@ class DivisionCalculator {
             int nextMultiplicationProduct = divisor * nextQuotient;
 
             if (i == 0) {
-
-                steps.append(Formatter.headStep(dividend, divisor, quotient, remainder, line, nextQuotient, nextMultiplicationProduct, dividendWidth));
+                steps.append(Formatter.headStep(dividend, divisor, quotient, line, nextMultiplicationProduct));
             } else {
-                steps.append(Formatter.midStep(dividend, divisor, quotient, remainder, nextDigit, nextQuotient, nextMultiplicationProduct, dividendWidth + (i + 1)));
+                steps.append(Formatter.midStep(nextDigit, nextMultiplicationProduct, i + 2));
             }
         }
-        steps.append(Formatter.lastStep( remainder, dividendWidth));
-
+        steps.append(Formatter.lastStep(remainder, dividendWidth + 1));
         System.out.print(steps);
     }
 }
 
 class Formatter {
-    public static String headStep(int dividend, int divisor, int quotient, int remainder, String line, int nextQuotient, int multiple, int width) {
+    public static String headStep(int dividend, int divisor, int quotient, String line, int multiple) {
         String dividendStr = String.valueOf(dividend);
         String divisorStr = String.valueOf(divisor);
         String quotientStr = String.valueOf(quotient);
-        String remainderStr = String.valueOf(remainder);
         String intermediateMultipleStr = String.valueOf(multiple);
-        String intermediateQuotient = String.valueOf(nextQuotient);
         int spaceCount = dividendStr.length();
+        String spaces = new String(new char[spaceCount - 1]).replace("\0", " ");
 
-        return String.format("%" + width + "s", "_" + dividendStr + "|" + divisorStr) + System.lineSeparator() +
-                String.format("%s%" + spaceCount + "s", " " + intermediateMultipleStr, "|")+line + System.lineSeparator() +
-                String.format("%" + width + "s", "--" + "|" )+ quotientStr + System.lineSeparator();
-
+        return String.format("%s", "_" + dividendStr + "|" + divisorStr) + System.lineSeparator() +
+                String.format("%s%" + spaceCount + "s", " " + intermediateMultipleStr, "|") + line + System.lineSeparator() +
+                String.format("%s", "--" + spaces + "|") + quotientStr + System.lineSeparator();
     }
 
-    public static String midStep(int dividend, int divisor, int quotient, int remainder, int nextDigit, int nextQuotient, int multiple, int width) {
-        String dividendStr = String.valueOf(dividend);
-        String divisorStr = String.valueOf(divisor);
-        String quotientStr = String.valueOf(quotient);
-
+    public static String midStep(int nextDigit, int multiple, int width) {
         String intermediateMultipleStr = String.valueOf(multiple);
         String intermediateDigitStr = String.valueOf(nextDigit);
-        String intermediateQuotient = String.valueOf(nextQuotient);
 
         return String.format("%" + width + "s", "_" + intermediateDigitStr) + System.lineSeparator() +
                 String.format("%" + width + "s", " " + intermediateMultipleStr) + System.lineSeparator() +
                 String.format("%" + width + "s", "--") + System.lineSeparator();
     }
 
-    public static String lastStep(int remainder, int width){
+    public static String lastStep(int remainder, int width) {
         String remainderStr = String.valueOf(remainder);
-       return String.format("%" + width + "s", " " + remainderStr);
-    }
 
+        return String.format("%" + width + "s", " " + remainderStr);
+    }
 }
